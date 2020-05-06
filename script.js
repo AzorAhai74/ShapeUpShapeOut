14360e4b11ed052c5bddf6c0898a0deb2a179589
Byron Baldwin <byronsbaldwin@gmail.com>
2020-05-05 21:47:12 -0500

    fixed jquery and css

--- script.js
+++ script.js
@@ -1,221 +1,189 @@
 $(document).ready(function() {
         
-    let $rectBtn = $("#rectBtn");
-    let $rectHinput = $('#rectHinput');
-    let $rectWinput = $('#rectWinput');
+	let $divContainer1 = $('<div></div>');
+	$($divContainer1).addClass('container shadow-lg border border-info');
 
-    let $squareBtn = $('#squareBtn');
-    let $squareInput = $('#squareInput');
+	let $divContainer2 = $('<div></div>');
+	$($divContainer2).addClass('container-fluid');
 
-    let $circleBtn = $('#circleBtn');
-    let $circleInput = $('#circleInput');
+	let $divRow = $('<div></div>');
+	$($divRow).addClass('row');
 
-    let $triangleBtn = $('#triangleBtn');
-    let $triangleInput = $('#triangleInput');
+	let $divCol1 = $('<div></div>');
+	$divCol1.addClass('col-md-2 shadow-lg border border-info p-4');
+	$($divCol1).css({
+		'margin': 'auto'
+	});
 
-    let $template = $('.template');
+	let $rectBtn = $('<button></button>');
+	$($rectBtn).addClass('btn btn-primary');
+	$($rectBtn).attr('id', 'rectHinput');
+	$($rectBtn).attr('id', 'rectWinput');
+	
+	let $squareBtn = $('<button></button>');
+	$($squareBtn).addClass('btn btn-primary');
+	$($squareBtn).attr('id', 'squareInput');
+	
+	let $circleBtn = $('<button></button>');
+	$($circleBtn).addClass('btn btn-primary');
+	$($circleBtn).attr('id', 'circleInput');
+	
+	let $triangleBtn = $('<button></button>');
+	$($triangleBtn).addClass('btn btn-primary');
+	$($triangleBtn).attr('id', 'triangleInput');
 
-    let $shapeInput = $('#shapeInput');
-    let $heightInput = $('#heightInput');
-    let $widthInput = $('#widthInput');
-    let $areaInput = $('#areaInput');
-    let $perInput = $('#perInput');
-    let $radiusInput = $('#radiusInput');
-        
-    $($rectBtn).css({
-        'box-shadow': '0px 10px 14px -7px #276873',
-	    'background': 'linear-gradient(to bottom, #F080805%, #408c99 100%)',
+	let $divTemplate = $('<div></div>');
+	$($divTemplate).addClass('template');
+	$($divTemplate).attr('id', 'shapeName');
+	$($divTemplate).css({
+		'height': '600px',
+		'width': '600px',
+		'margin': 'auto',
+		'background-color': 'lightskyblue'
+	});
+	
+
+	let $divCol2 = $('<div></div>');
+	$($divCol2).addClass('.col-md-2 shadow-lg border border-info p-4');
+	$($divCol2).attr('id', 'shapeInput');
+	$($divCol2).attr('id', 'heightInput');
+	$($divCol2).attr('id', 'widthInput');
+	$($divCol2).attr('id', 'areaInput');
+	$($divCol2).attr('id', 'perInput');
+	$($divCol2).attr('id', 'radiusInput');
+	$($divCol2).css({
+		'margin': 'auto'
+	});
+	
+	
+
+	
+	$($divRow).append($divCol1);
+	$($divRow).append($divTemplate);
+	$($divRow).append($divCol2);
+	$($rectBtn).appendTo($divCol1);
+	$($squareBtn).appendTo($divCol1);
+	$($circleBtn).appendTo($divCol1);
+	$($triangleBtn).appendTo($divCol1);
+	$($divContainer2).append($divRow);
+	$('body').append($divContainer2);
+	
+
+	        
+	$($rectBtn).css({
+		'background': 'linear-gradient(to bottom, #F080805%, #408c99 100%)',
+		'background-color': '#F08080',
+	    'border-radius': '6px',
+	    'width': '140px',
+	    'height': '35px',
+		'cursor': 'pointer',
+		'color': '#ffffff',
+		'font-family': 'Arial',
+		'font-size': '12px',
+		'font-weight': 'bold',
+		'padding': '10px 25px',
+		'text-decoration': 'none',
+	    'text-shadow': '0px 1px 0px #3d768a',
 	    'background-color': '#F08080',
-        'border-radius': '6px',
-        'width': '140px',
-        'height': '35px',
-	    'cursor': 'pointer',
-	    'color': '#ffffff',
-	    'font-family': 'Arial',
-	    'font-size': '12px',
-	    'font-weight': 'bold',
-	    'padding': '10px 25px',
-	    'text-decoration': 'none',
-        'text-shadow': '0px 1px 0px #3d768a',
-        'background' :'linear-gradient(to bottom,  #F08080 5%, #599bb3 100%)',
-        'background-color': '#F08080',
-        'position': 'static',
-        'top': '1px',
-        'margin': '3px'
-    });
-
-    $($squareBtn).css({
-        'box-shadow': '0px 10px 14px -7px #276873',
-	    'background': 'linear-gradient(to bottom, #F080805%, #408c99 100%)',
+	    'position': 'static',
+	    'top': '1px',
+	    'margin': '3px'
+	});
+	
+	$($squareBtn).css({
+		'background': 'linear-gradient(to bottom, #F080805%, #408c99 100%)',
+		'background-color': '#F08080',
+	    'border-radius': '6px',
+	    'width': '140px',
+	    'height': '35px',
+		'cursor': 'pointer',
+		'color': '#ffffff',
+		'font-family': 'Arial',
+		'font-size': '12px',
+		'font-weight': 'bold',
+		'padding': '10px 25px',
+		'text-decoration': 'none',
+	    'text-shadow': '0px 1px 0px #3d768a',
 	    'background-color': '#F08080',
-        'border-radius': '6px',
-        'width': '140px',
-        'height': '35px',
-	    'cursor': 'pointer',
-	    'color': '#ffffff',
-	    'font-family': 'Arial',
-	    'font-size': '12px',
-	    'font-weight': 'bold',
-	    'padding': '10px 25px',
-	    'text-decoration': 'none',
-        'text-shadow': '0px 1px 0px #3d768a',
-        'background' :'linear-gradient(to bottom,  #F08080 5%, #599bb3 100%)',
-        'background-color': '#F08080',
-        'position': 'static',
-        'top': '1px',
-        'margin': '3px'
-    });
-
-    $($circleBtn).css({
-        'box-shadow': '0px 10px 14px -7px #276873',
-	    'background': 'linear-gradient(to bottom, #F080805%, #408c99 100%)',
+	    'position': 'static',
+	    'top': '1px',
+	    'margin': '3px'
+	});
+	
+	$($circleBtn).css({
+		'background': 'linear-gradient(to bottom, #F080805%, #408c99 100%)',
+		'background-color': '#F08080',
+	    'border-radius': '6px',
+	    'width': '140px',
+	    'height': '35px',
+		'cursor': 'pointer',
+		'color': '#ffffff',
+		'font-family': 'Arial',
+		'font-size': '12px',
+		'font-weight': 'bold',
+		'padding': '10px 25px',
+		'text-decoration': 'none',
+	    'text-shadow': '0px 1px 0px #3d768a',
 	    'background-color': '#F08080',
-        'border-radius': '6px',
-        'width': '140px',
-        'height': '35px',
-	    'cursor': 'pointer',
-	    'color': '#ffffff',
-	    'font-family': 'Arial',
-	    'font-size': '12px',
-	    'font-weight': 'bold',
-	    'padding': '10px 25px',
-	    'text-decoration': 'none',
-        'text-shadow': '0px 1px 0px #3d768a',
-        'background' :'linear-gradient(to bottom,  #F08080 5%, #599bb3 100%)',
-        'background-color': '#F08080',
-        'position': 'static',
-        'top': '1px',
-        'margin': '3px'
-    });
-
-    $($triangleBtn).css({
-        'box-shadow': '0px 10px 14px -7px #276873',
-	    'background': 'linear-gradient(to bottom, #F080805%, #408c99 100%)',
+	    'position': 'static',
+	    'top': '1px',
+	    'margin': '3px'
+	});
+	
+	$($triangleBtn).css({
+		'background': 'linear-gradient(to bottom, #F080805%, #408c99 100%)',
+		'background-color': '#F08080',
+	    'border-radius': '6px',
+	    'width': '140px',
+	    'height': '35px',
+		'cursor': 'pointer',
+		'color': '#ffffff',
+		'font-family': 'Arial',
+		'font-size': '12px',
+		'font-weight': 'bold',
+		'padding': '10px 25px',
+		'text-decoration': 'none',
+	    'text-shadow': '0px 1px 0px #3d768a',
 	    'background-color': '#F08080',
-        'border-radius': '6px',
-        'width': '140px',
-        'height': '35px',
-	    'cursor': 'pointer',
-	    'color': '#ffffff',
-	    'font-family': 'Arial',
-	    'font-size': '12px',
-	    'font-weight': 'bold',
-	    'padding': '10px 25px',
-	    'text-decoration': 'none',
-        'text-shadow': '0px 1px 0px #3d768a',
-        'background' :'linear-gradient(to bottom,  #F08080 5%, #599bb3 100%)',
-        'background-color': '#F08080',
-        'position': 'static',
-        'top': '1px',
-        'margin': '3px'
-    });
-
-    $($rectHinput).css({
-        'width': '175px',
-        'margin': '5px'
-    });
-
-    $($rectWinput).css({
-        'width': '175px',
-        'margin': '5px'
-    });
-
-    $($squareInput).css({
-        'width': '175px',
-        'margin': '5px'
-    });
-
-    $($circleInput).css({
-        'width': '175px',
-        'margin': '5px'
-    });
-
-    $($triangleInput).css({
-        'width': '175px',
-        'margin': '5px'
-    });
-
-    $($template).css({
-        'width': '600px',
-        'height': '600px',
-        'background-color': 'lightskyblue',
-        'margin': '100px',
-        'margin-top': '10px'
-    });
-
-    $($shapeInput).css({
-        'width': '175px',
-        'margin': '5px',
-        'margin-bottom': '30px'
-    });
-
-    $($heightInput).css({
-        'width': '175px',
-        'margin': '5px'
-    });
-
-    $($widthInput).css({
-        'width': '175px',
-        'margin': '5px',
-        'margin-top': '30px'
-    });
-
-    $($areaInput).css({
-        'width': '175px',
-        'margin': '5px',
-        'margin-top': '30px'
-    });
-
-    $($perInput).css({
-        'width': '175px',
-        'margin': '5px',
-        'margin-top': '30px'
-    });
-
-    $($radiusInput).css({
-        'width': '175px',
-        'margin': '5px',
-        'margin-top': '30px'
-    });
-
-    class Shape {
-        constructor(width, height, background) {
-            this.width = width;
-            this.height = height;
-            this.background = background;
-        };
-    };
-
-
-    class Rectangle extends Shape {
-        constructor($width, $height) {
-        super($width, $height, $background);
-        this.$width = '200px'
-        this.$height = '100px'
-        
-        }
-
-        
-        
-    }
-
-    class Square {
-        constructor() {
-
-        }
-    }
-
-    class Circle {
-        constructor() {
-
-        }
-    }
-
-    class Triangle {
-        constructor() {
-
-        }
-    }
+	    'position': 'static',
+	    'top': '1px',
+	    'margin': '3px'
+	});
+	
+	
+	
+	
+	
+	class Shape {
+	    constructor() {
+			
+	    }
+	};
+	
+	
+	class Rectangle {
+	    constructor() {
+	        
+	    }
+	
+	}
+	
+	class Square {
+	    constructor() {
+	    }
+	}
+	
+	class Circle {
+	    constructor() {
+	
+	    }
+	}
+	
+	class Triangle {
+	    constructor() {
+	
+	    }
+	}
 
 
 
